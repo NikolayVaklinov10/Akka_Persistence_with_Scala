6e7a010b0cb7bef874a347e3267c24330648ee93
Nick <nikolayvaklinov@yahoo.com>
2020-08-11 19:48:06 +0100

    the exercise containing the voting system

---
+++ src/main/scala/part2_event_sourcing/PersistentActorsExercise.scala
@@ -0,0 +1,51 @@
+package part2_event_sourcing
+
+import akka.actor.ActorLogging
+import akka.persistence.PersistentActor
+
+import scala.collection.mutable
+
+object PersistentActorsExercise extends App {
+
+  /*
+
+   */
+  case class Vote(citizenPID: String, candidate: String)
+
+  class VotingStation extends PersistentActor with ActorLogging {
+
+    // ignore the mutable state for now
+    val citizens: mutable.Set[String] = new mutable.HashSet[String]()
+    val poll: mutable.Map[String, Int] = new mutable.HashMap[String, Int]()
+
+    override def persistenceId: String = "simple-voting-station"
+
+    override def receiveCommand: Receive = {
+      case vote @ Vote(citizenPID, candidate) =>
+        /*
+        follow the pattern
+
+        1) create the event
+        2) persist the event
+        3) handle a state change after persisting is successful
+         */
+        persist(vote) { _ => // COMMAND sourcing
+          handleInternalStateChange(vote)
+        }
+      case "print" =>
+        log.info("Current state: ")
+    }
+
+    def handleInternalStateChange(vote: Vote): Unit = {
+      if (!citizens.contains(vote.citizenPID)) {
+        citizens.add(vote.citizenPID)
+        val votes = poll.getOrElse(vote.candidate, 0)
+        poll.put(vote.candidate, votes + 1)
+      }
+    }
+
+    override def receiveRecover: Receive = ???
+
+  }
+
+}
