645f1811df6fd160dfc896eaf58c12bdd4267631
Nick <nikolayvaklinov@yahoo.com>
2020-10-19 21:51:04 +0100

    monads explained

---
+++ src/main/scala/playground/Monads.scala
@@ -0,0 +1,8 @@
+package playground
+
+object Monads extends App {
+
+  // in simple term Monad is a way to make a function specify the transformation
+  // of a particular way making a side effect to be effect
+
+}
