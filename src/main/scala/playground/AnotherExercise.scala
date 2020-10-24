f8fe4d8c9a9aa81980e5ec69a33d5e038f4ad06f
Nick <nikolayvaklinov@yahoo.com>
2020-10-24 22:02:37 +0100

    checking the indentation option of scala

---
+++ src/main/scala/playground/AnotherExercise.scala
@@ -0,0 +1,14 @@
+package playground
+
+object AnotherExercise extends App {
+
+  def incrementer(a:Double, b:Double):Double = {
+     a + b
+  }
+
+
+  println("My total number is "+incrementer(3.4,4.6)+ " the indented incrementer function works")
+
+  
+
+}
