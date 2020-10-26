c410859613afd92a9bdac1363c1dfa1dfdd3e341
Nick <nikolayvaklinov@yahoo.com>
2020-10-26 23:32:12 +0000

    a cool trick

--- src/main/scala/playground/AnotherExercise.scala
+++ src/main/scala/playground/AnotherExercise.scala
@@ -3,12 +3,13 @@ package playground
 object AnotherExercise extends App {
 
   def incrementer(a:Double, b:Double):Double = {
-     a + b
+    a + b
   }
-
-
   println("My total number is "+incrementer(3.4,4.6)+ " the indented incrementer function works")
-//  Note use dotty for using the new features of scala 3
-  
+  //  Note use dotty for using the new features of scala 3
+  // shift command arrow to move the part selected
+
+
+
 
 }
